7cc2a7a8197fa4c4f510f23705c0c02efe01485f
Meek Karan <meekkaran@gmail.com>
2021-03-11 23:54:05 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBContainer } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './style.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    const container = { height: 1300 }
+    return (
+      <div className="topbar">
+        <Router>
+          <header>
+            <MDBNavbar color="default-color" dark expand="md">
+              <MDBNavbarBrand href="/">
+                <strong className="logo">NG@23 <i className="fab fa-wpexplorer"></i></strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.onClick} />
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Features</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Pricing</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Opinions</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="instagram" /></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
+        <MDBContainer style={container} className="text-center mt-5">
+          <h2>This Navbar isn't fixed</h2>
+          <h5>When you scroll down it will disappear</h5>
+        </MDBContainer>
+      </div>
+    );
+  }
+}
+
+export default Header;
